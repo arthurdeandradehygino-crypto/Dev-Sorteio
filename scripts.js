bd298b8164a9bf34b7de6e6bf2decce6e4aefe3c
arthurdeandradehygino-crypto <arthurdeandradehygino@gmail.com>
2026-01-17 12:56:44 -0300

    add JavaScript

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
 
+const button= document.querySelector(".button")
+
+
+function sortear (){
+
+const min= Math.ceil(document.querySelector(".min").value)
+const max= Math.floor(document.querySelector(".max").value)
+
+const result= Math.floor(Math.random() * (max - min + 1)) + min;
+
+alert(result)
+
+}
 
 
 
+document.addEventListener("keypress", function(e){
+
+if(e.key === 'Enter')
+
+sortear()
+
+})
+
+button.addEventListener("click", sortear)
 
 
 
