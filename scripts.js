21f9c036a04ca44b2d1bdd7ce1ef91d5cc39f50a
arthurdeandradehygino-crypto <arthurdeandradehygino@gmail.com>
2026-01-18 06:58:07 -0300

    pequena alteração no js

--- scripts.js
+++ scripts.js
@@ -7,9 +7,23 @@ function sortear (){
 const min= Math.ceil(document.querySelector(".min").value)
 const max= Math.floor(document.querySelector(".max").value)
 
+if(max > min){
+
+
 const result= Math.floor(Math.random() * (max - min + 1)) + min;
 
 alert(result)
+
+
+}
+
+else{
+
+alert("o valor minimo tem que ser MENOR que o valor maximo")
+
+    
+}
+
 
 }
 
